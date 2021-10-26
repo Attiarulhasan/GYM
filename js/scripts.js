46a6e93640a67fdd83f136a04e5eedd2fe161786
mianattiar <mianattiar@gmail.com>
2021-10-26 17:55:19 +0500

    GYMe

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.uneven').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
+});
